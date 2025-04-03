4b27fe82e6b97d8b7db206416dd02707f6b4f9c3
Veljko Stojadinovic <e11835430@student.tuwien.ac.at>
2025-04-03 17:19:55 +0200

    Testing (:

--- src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,12 +1,14 @@
 package at.ac.fhcampuswien.fhmdb;
 
 import at.ac.fhcampuswien.fhmdb.models.Movie;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HomeControllerTest {
@@ -23,12 +25,36 @@ class HomeControllerTest {
     @Test
     public void testGetReleaseYearsFromMovies() {
         Set<Integer> releaseYearsFromMovies = homeController.getReleaseYearsFromMovies(movies.subList(0, 2));
+        Assertions.assertFalse(releaseYearsFromMovies.contains(2020));
+        Assertions.assertFalse(releaseYearsFromMovies.contains(2022));
+        Assertions.assertTrue(releaseYearsFromMovies.contains(2010));
+        Assertions.assertEquals(2, releaseYearsFromMovies.size());
     }
 
     @Test
     public void testGetRatingsFromMovies() {
         Set<Double> ratingsFromMovies = homeController.getRatingsFromMovies(movies.subList(3, 6));
+        Assertions.assertEquals(3, ratingsFromMovies.size());
+        Assertions.assertTrue(ratingsFromMovies.contains(8.6));
+        Assertions.assertTrue(ratingsFromMovies.contains(9.2));
+        Assertions.assertFalse(ratingsFromMovies.contains(4.2));
 
+    }
+
+    @Test
+    public void testCountMoviesFrom() {
+        long count = homeController.countMoviesFrom(movies, "Christopher Nolan");
+        assertEquals(4, count);
+    }
+
+    // test für getMoviesBetweenYears
+    @Test
+    public void testGetMoviesBetweenYears() {
+        List<Movie> moviesBetweenYears = homeController.getMoviesBetweenYears(movies, 1990, 2000);
+        assertEquals(6, moviesBetweenYears.size());
+        Assertions.assertTrue(moviesBetweenYears.stream().allMatch(movie -> movie.getReleaseYear() >= 1990 && movie.getReleaseYear() <= 2000));
+        Assertions.assertTrue(movies.size() > moviesBetweenYears.size());
+        Assertions.assertNotEquals(movies.size(), moviesBetweenYears.size());
     }
 
     @Test
@@ -39,19 +65,9 @@ class HomeControllerTest {
 
     @Test
     public void testGetLongestMovieTitle() {
-
+        int longestMovieTitle = homeController.getLongestMovieTitle(movies);
+        Assertions.assertEquals(45, longestMovieTitle);
     }
 
-    @Test
-    public void testCountMoviesFrom() {
-        long countMoviesFrom = homeController.countMoviesFrom(movies, "Inception");
-
-    }
-
-    @Test
-    public void testGetMoviesBetweenYears() {
-        List<Movie> result = homeController.getMoviesBetweenYears(movies, 1900, 2000);
-        assertEquals(7, result.size());
-    }
 
 }
